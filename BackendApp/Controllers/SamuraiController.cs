e437b4347049e6c8256f9d975477d24c69c6c48c
unknown <ifmanullang@gmail.com>
2023-03-14 20:10:29 +0700

    membuat api dotnet

---
+++ BackendApp/Controllers/SamuraiController.cs
@@ -0,0 +1,22 @@
+﻿using BackendApp.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BackendApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SamuraiController : ControllerBase
+    {
+        [HttpGet]
+        public Samurai Get()
+        {
+            Samurai samurai1 = new Samurai() 
+            {
+                Id = 1,
+                Name = "Sasaki Tajuro"
+            };
+            return samurai1;
+        }
+    }
+}
